220697da05770cebb75a6d887b83c41ef846da77
dfe09 <daniel.feurle@gw-world.com>
2022-06-25 19:12:24 +0200

    add empty test

--- mydocs-producer/src/test/java/com/feurle/mydocs/producer/MydocsProducerApplicationTests.java
+++ mydocs-producer/src/test/java/com/feurle/mydocs/producer/MydocsProducerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.feurle.mydocs.producer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class MydocsProducerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
