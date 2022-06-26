96c3ebe50cd2ed3838758cf02f5a1a64a01637c2
Daniel Feurle <feurle@e.email>
2022-06-26 07:44:47 +0200

    add kafka config for producing messages

--- mydocs-producer/src/main/java/com/feurle/mydocs/producer/service/MyDocumentGenerationService.java
+++ mydocs-producer/src/main/java/com/feurle/mydocs/producer/service/MyDocumentGenerationService.java
@@ -1,7 +1,10 @@
 package com.feurle.mydocs.producer.service;
 
+import com.feurle.mydocs.producer.domain.MyDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +13,22 @@ public class MyDocumentGenerationService {
 
     private final Logger log = LoggerFactory.getLogger(MyDocumentGenerationService.class);
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, MyDocument> mydocsKafkaTemplate;
+
+    private void sendSimpleMessage(String msg) {
+        kafkaTemplate.send("feurle", msg);
+    }
+
     @Scheduled (fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
+        long tm = System.currentTimeMillis() / 1000;
         log.info(
-                "Fixed delay task - {}", System.currentTimeMillis() / 1000);
+                "Fixed delay task - {}", tm);
+        sendSimpleMessage(String.valueOf(tm));
     }
 
     @Scheduled(fixedRate = 1000)
